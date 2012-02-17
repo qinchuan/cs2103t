7a2064924b965c0118d62310af4f04cea7de3cc4
Zhang Xi <a0091390@nus.edu.sg>
2012-02-17 23:23:30 +0800

    Done with Facility

--- NON-GUI/src/team34/Main.java
+++ NON-GUI/src/team34/Main.java
@@ -1,5 +1,9 @@
 package team34;
 
+import java.util.LinkedList;
+import java.util.Set;
+
+import team34.event.facility.*;
 import team34.student.*;
 public class Main {
 
@@ -8,14 +12,27 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
-		Student stu = new Student();
-		
-		Faciliator f=stu;
-		System.out.println();
-		//System.out.println();
-	
-		
+	 VenueManager mang = new VenueManager();
+	 
+	 mang.bookVenue("Science", "LT33", new Schedule("18 Feb",12,14));
+	 mang.bookVenue("Science", "LT33", new Schedule("18 Feb",13,15));
+	 mang.bookVenue("Science", "LT33", new Schedule("18 Feb",10,12));
+	 mang.bookVenue("Science", "LT33", new Schedule("18 Feb",14,16));
+	 mang.bookVenue("Science", "LT33", new Schedule("18 Feb",16,18));
+	 
+	Set<String> typeList = mang.getTypeListOf("Science");
+	 for(String cur:typeList){
+		 System.out.println(cur);
+	 }
+	 
+	 System.out.println();
+	 
+	 mang.cancelBooking("Science", "LT33",new Schedule("18 Feb",14,16));
+	 
+	 LinkedList<Schedule> changed = mang.getRoom("Science", "LT33").getSchedules();
+	 for(Schedule cur:changed){
+		 System.out.println(cur.toString());
+	 }
 	}
 
 }
